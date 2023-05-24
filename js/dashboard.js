e37e7dc39d419ab95fb5d1ca00cae3a5526b9c79
solohosein <hosein@bostonasiacapital.com>
2023-05-24 08:50:03 -0400

    posting message to worker

--- js/dashboard.js
+++ js/dashboard.js
@@ -852,15 +852,16 @@ const Tables = {
 
 function main() {
   // const st = performance.now();
-  // window.open('/','_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none', ''); 
-
-  const parentWorker = new Worker("/static/js/testing.js");
+  // window.open('/','_blank', 'toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none', '');
 
   Constants.Ipaddress = DocElems.statchartOpts.innerHTML;
   DocElems.statchartOpts.innerHTML = "";
   Constants.Origin = "http://" + Constants.Ipaddress + ":50000/";
   Constants.RequestWSUrl =
     "ws://" + Constants.Ipaddress + Constants.RequestWSExt;
+
+  const parentWorker = new Worker("/static/js/websocket.js");
+  parentWorker.postMessage(Constants.Ipaddress);
 
   let onloadfilters = new Map(JSON.parse(localStorage.getItem("radarfilters")));
   if (onloadfilters.size > 0) {
