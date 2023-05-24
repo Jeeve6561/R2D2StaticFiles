647a95a6967651d3c3d5a74eb0195c29cc3ea90f
solohosein <hosein@bostonasiacapital.com>
2023-05-24 10:42:33 -0400

    Debugged grafana

--- js/grafana.js
+++ js/grafana.js
@@ -1,5 +1,5 @@
 function main() {
-    window.open("http://192.168.0.35:3000/d/a8a7d7a8-641a-471c-b781-101855d4ce2e/hal-system?orgId=1&refresh=5s&from=1684937469588&to=1684938369588&theme=dark");
+    window.open("http://192.168.0.35:3000/d/a8a7d7a8-641a-471c-b781-101855d4ce2e/hal-system?orgId=1&refresh=5s&from=now-15m&to=now");
 }
 
 main();
