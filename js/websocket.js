fd1cf19dc6ce849f81ffd9da055c0c9259c719fc
solohosein <hosein@bostonasiacapital.com>
2023-05-24 09:28:39 -0400

    debugging

--- js/websocket.js
+++ js/websocket.js
@@ -110,7 +110,7 @@ const LiveFeedWS = {
 
 function main() {
   onmessage = (event) => {
-    indexedDB = typeof window == "object" ? window.indexedDB : webkitIndexedDB;
+    // indexedDB = typeof window == "object" ? window.indexedDB : webkitIndexedDB;
     Constants.Ipaddress = event.data;
     CreateAndInitAllDatabases([Constants.Database.Store.name]);
     Constants.LiveFeedWSUrl =
