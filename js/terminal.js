7edff725a7a7884273808f107944e9b91d980e1c
solohosein <hosein@bostonasiacapital.com>
2023-05-24 12:40:59 -0400

    terminal shell code

--- js/terminal.js
+++ js/terminal.js
@@ -1,11 +1,100 @@
+const Constants = {
+  Ipaddress: "",
+  TerminalWSExt: ":44444/terminalstream",
+  TerminalWSUrl: "",
+  TerminalOutput: "terminaloutput",
+  ConnectMsg: "connected",
+};
+
+const DocElems = {
+  Ipaddress: document.getElementById("ipaddress"),
+};
+
+const TerminalWS = {
+  init: false,
+  connected: false,
+  connectTime: 0,
+  disconnectTime: 0,
+  ws: null,
+
+  connect() {
+    TerminalWS.connected = true;
+    TerminalWS.url = Constants.TerminalWSUrl;
+    TerminalWS.ws = new WebSocket(TerminalWS.url);
+    TerminalWS.ws.onopen = TerminalWS.openSocket;
+    TerminalWS.ws.onclose = TerminalWS.closeSocket;
+    TerminalWS.ws.onerror = TerminalWS.errorSocket;
+    TerminalWS.ws.onmessage = TerminalWS.messageReceived;
+  },
+
+  openSocket() {
+    TerminalWS.connected = true;
+    TerminalWS.connectTime = new Date();
+    console.log(
+      "Connecting to " +
+        String(TerminalWS.url) +
+        " at " +
+        String(TerminalWS.connectTime)
+    );
+  },
+
+  closeSocket() {
+    TerminalWS.connected = false;
+    TerminalWS.disconnectTime = new Date();
+    console.log(
+      "Disconnecting from " + String(TerminalWS.url) + " at ",
+      String(TerminalWS.disconnectTime)
+    );
+    TerminalWS.ws = null;
+  },
+
+  errorSocket(event) {
+    console.error(
+      "Error occurred in " +
+        String(TerminalWS.url) +
+        " at " +
+        String(new Date()) +
+        " | " +
+        event.data
+    );
+  },
+
+  messageReceived(event) {
+    let msg = JSON.parse(event.data);
+    switch (msg.ev) {
+      case Constants.ConnectMsg:
+        console.log("Name of socket manager:", msg.n);
+        break;
+      case Constants.TerminalOutput:
+        HandleTerminalOutput(msg.d);
+        break;
+    }
+  },
+
+  sendMessage(msg) {
+    if (TerminalWS.ws.readyState === WebSocket.OPEN && TerminalWS.connected) {
+      let data = JSON.stringify(msg);
+      TerminalWS.ws.send(data);
+    } else {
+      console.error("Failed to send and retrying");
+      setTimeout(() => {
+        TerminalWS.sendMessage(msg);
+      }, 1000);
+    }
+  },
+};
+
 function main() {
-  let ipaddress = document.getElementById("ipaddress").innerHTML;
-  document.getElementById("ipaddress").innerHTML = "";
-  let url = "ws://" + ipaddress + ":50001/socket.io/?EIO=3&transport=websocket";
-  var terminal = new WebTTY({
-    target: document.getElementById("terminal"),
-    socketUrl: ipaddress,
-  });
+  Constants.Ipaddress = DocElems.Ipaddress.innerHTML;
+  DocElems.Ipaddress.innerHTML = "";
+  Constants.TerminalWSUrl =
+    "ws://" + Constants.Ipaddress + Constants.TerminalWSExt;
+
+  TerminalWS.connect();
+}
+
+function HandleTerminalOutput(data) {
+  console.log(data);
 }
 
 main();
