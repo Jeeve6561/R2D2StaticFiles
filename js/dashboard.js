589e8118d07eb5961f82255514a18b5af03ef055
solohosein <hosein@bostonasiacapital.com>
2023-05-24 09:21:02 -0400

    removing indexeddb from dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -870,16 +870,16 @@ function main() {
 
   RequestWS.connect();
 
-  const request = indexedDB.open(
-    Constants.Database.Name,
-    Constants.Database.Version
-  );
-  request.onsuccess = (event) => {
-    Constants.Db = request.result;
-  };
-  request.onerror = (event) => {
-    console.error("Couldn't open database");
-  };
+  // const request = indexedDB.open(
+  //   Constants.Database.Name,
+  //   Constants.Database.Version
+  // );
+  // request.onsuccess = (event) => {
+  //   Constants.Db = request.result;
+  // };
+  // request.onerror = (event) => {
+  //   console.error("Couldn't open database");
+  // };
 
   CanvasCharts.Stock.render();
   CanvasCharts.Stock.charts[0].container
