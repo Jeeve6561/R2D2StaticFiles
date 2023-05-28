7dc9533f88f357656c3746c20d511a96894a986d
solohosein <hosein@bostonasiacapital.com>
2023-05-28 05:54:21 -0400

    debugging

--- js/dashboard.js
+++ js/dashboard.js
@@ -1595,13 +1595,13 @@ function UpdateRadarChart() {
 }
 
 function UpdateIndicatorTable() {
-  console.log("In here");
   if (
     GraphData.radardata === undefined ||
     GraphData.radardata.length === 0
   ) {
     return;
   }
+  console.log("In here");
   if (GraphData.ChangeIndicatorTitle) {
     DocElems.indicatorquan.value = GraphData.IQuan;
 
